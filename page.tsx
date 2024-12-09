07fa9dcf5fa4bc3537eb92269ac97382d1001d7f
ayeshaabro25 <abroayesha45@gmail.com>
2024-12-09 11:47:49 -0800

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,46 +1,27 @@
-"use client";
+// In src/app/page.tsx
+import React from 'react';
+import Hero from './components/Hero';
+import ProductGrid from './components/ProductGrid';
+import NeutralUniverse from './components/NeutralUniverse';
+import FeaturedPost from './components/FeaturedPosts';
+import EditorPickGrid from './components/EditorsPick';
+import VitsClassic from './components/VitsClassic';
+import Breadcrumb from './components/BreadCrumb';
 
-import React from "react";
-import styles from "./contact.module.css";
 
-const Contact: React.FC = () => {
+const HomePage: React.FC = () => {
   return (
-    <div className={styles.contactContainer}>
-      <h1 className={styles.title}>Contact Us</h1>
-      <div className={styles.contactInfo}>
-        <div className={styles.infoItem}>
-          <h3>Phone</h3>
-          <p>(225) 555-0118</p>
-        </div>
-        <div className={styles.infoItem}>
-          <h3>Email</h3>
-          <p>michelle.rivera@example.com</p>
-        </div>
-        <div className={styles.infoItem}>
-          <h3>Address</h3>
-          <p>123 Example Street, Springfield, USA</p>
-        </div>
-      </div>
-      <div className={styles.followUs}>
-        <h3>Follow Us</h3>
-        <div className={styles.socialLinks}>
-          <a href="#" className={styles.socialIcon}>
-            Instagram
-          </a>
-          <a href="#" className={styles.socialIcon}>
-            YouTube
-          </a>
-          <a href="#" className={styles.socialIcon}>
-            Facebook
-          </a>
-          <a href="#" className={styles.socialIcon}>
-            Twitter
-          </a>
-        </div>
-      </div>
+    <div>
+      <Hero/>
+      <Breadcrumb/>
+      <EditorPickGrid/>
+      <ProductGrid />
+      <VitsClassic/>
+      <NeutralUniverse />
+      <FeaturedPost/>
     </div>
   );
 };
 
-export default Contact;
+export default HomePage;
 
